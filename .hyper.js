43782384d8e0b0eaeeaaa75184a557fa288dd9b5
Martin van Driel <martinvd@gmail.com>
2018-05-15 20:01:05 +0200

    Change hyper theme

--- .hyper.js
+++ .hyper.js
@@ -116,7 +116,7 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: ['hyper-snazzy', "hypercwd"],
+  plugins: ['hyperterm-atom-dark', 'hypercwd'],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
